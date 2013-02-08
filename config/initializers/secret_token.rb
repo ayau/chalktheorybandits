905fc68beb1f4bc2c9d1aec329dbdba5872fd3d6
David Stahl <drs2010@sbcglobal.net>
2013-02-07 23:33:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandproject::Application.config.secret_token = '41c8747bb31e0d8b323b83210156d83ec0e1443c2f32c2677457080a7fbc4301e92d7f9bd88a94326d41d3b619ca78cb8f9dca46c15fc875ae4e71ae3724ad00'
